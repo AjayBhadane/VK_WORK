52627826d76ee3b63d586fb7a2b11900bbfe9112
Catastrophe <ajaybhadane95@gmail.com>
2022-01-12 11:40:01 +0000

    Cleanup

--- src/vkutil.hpp
+++ src/vkutil.hpp
@@ -150,8 +150,18 @@ bool checkDeviceExtensionSupport(VkPhysicalDevice device){
 
     uint32_t extensionCount;
     vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, nullptr);
-
+        
     std::vector<VkExtensionProperties> availableExtensions(extensionCount);
+    vkEnumerateDeviceExtensionProperties(device, nullptr, &extensionCount, availableExtensions.data());
+
+    std::set<std::string> requiredExtensions (deviceExtensions.begin(), deviceExtensions.end());
+
+
+    for(const auto& extension : availableExtensions){
+        requiredExtensions.erase(extension.extensionName);
+    }
+
+    return requiredExtensions.isEmpty();
 }
 
 inline bool isDeviceSuitable(VkPhysicalDevice device, VkSurfaceKHR surface = NULL){
